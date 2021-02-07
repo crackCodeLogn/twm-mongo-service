e2f360a703e853634a5cb0c5405b064e184f2c74
Vivek <vivektheintel@gmail.com>
2021-02-07 13:41:44 +0530

    Incorporating pinging mech for end-points check

--- src/main/java/com/vv/personal/twm/mongo/MongoService.java
+++ src/main/java/com/vv/personal/twm/mongo/MongoService.java
@@ -1,14 +1,26 @@
 package com.vv.personal.twm.mongo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static com.vv.personal.twm.mongo.constants.Constants.*;
 
 /**
  * @author Vivek
@@ -16,8 +28,14 @@ import springfox.documentation.spring.web.plugins.Docket;
  */
 @EnableFeignClients
 @EnableEurekaClient
+@ComponentScan({"com.vv.personal.twm.mongo", "com.vv.personal.twm.ping"})
 @SpringBootApplication
 public class MongoService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MongoService.class);
+
+    @Autowired
+    private Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(MongoService.class, args);
     }
@@ -33,5 +51,18 @@ public class MongoService {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.vv.personal.twm.mongo"))
                 .build();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void firedUpAllCylinders() {
+        String host = LOCALHOST;
+        try {
+            host = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOGGER.error("Failed to obtain ip address. ", e);
+        }
+        String port = environment.getProperty(LOCAL_SPRING_PORT);
+        LOGGER.info("'{}' activation is complete! Exact url: {}", environment.getProperty("spring.application.name").toUpperCase(),
+                String.format(SWAGGER_UI_URL, host, port));
     }
 }

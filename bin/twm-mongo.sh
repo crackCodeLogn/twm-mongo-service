000539e6359e31f2e889da906e013615bc3a342a
Vivek <vivektheintel@gmail.com>
2023-11-23 15:45:19 -0500

    Upgrading spring, swagger, shell script, app version

--- bin/twm-mongo.sh
+++ bin/twm-mongo.sh
@@ -1,11 +1,12 @@
 APP_NAME="twm-mongo-service"
-APP_VERSION="1.0-SNAPSHOT"
+APP_VERSION="2.0-SNAPSHOT"
 JAVA_PARAM="-Xmx1g"
 TWM_PARAM=""
 
 BIN_PATH=$TWM_HOME_PARENT/TWM/$APP_NAME/bin     #TWM-HOME-PARENT :: exported in .bashrc
 JAR_PATH=$BIN_PATH/../target/$APP_NAME-$APP_VERSION.jar
 PARAMS=$JAVA_PARAM" "$TWM_PARAM
+JAVA_PATH=$HOME/.jdks/corretto-17.0.9/bin/java
 
 echo "Starting '$APP_NAME' with params: '$PARAMS', at '$JAR_PATH'"
-java $PARAMS -jar $JAR_PATH
+$JAVA_PATH $PARAMS -jar $JAR_PATH
--- src/main/java/com/vv/personal/twm/mongo/MongoService.java
+++ src/main/java/com/vv/personal/twm/mongo/MongoService.java
@@ -6,19 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.time.ZoneId;
+import java.util.TimeZone;
 
 import static com.vv.personal.twm.mongo.constants.Constants.*;
 
@@ -27,7 +26,7 @@ import static com.vv.personal.twm.mongo.constants.Constants.*;
  * @since 16/11/20
  */
 @EnableFeignClients
-@EnableEurekaClient
+@EnableDiscoveryClient
 @ComponentScan({"com.vv.personal.twm.mongo", "com.vv.personal.twm.ping"})
 @SpringBootApplication
 public class MongoService {
@@ -37,20 +36,13 @@ public class MongoService {
     private Environment environment;
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("EST", ZoneId.SHORT_IDS))); //force setting
         SpringApplication.run(MongoService.class, args);
     }
 
     @Bean
     ProtobufHttpMessageConverter protobufHttpMessageConverter() {
         return new ProtobufHttpMessageConverter();
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.vv.personal.twm.mongo"))
-                .build();
     }
 
     @EventListener(ApplicationReadyEvent.class)

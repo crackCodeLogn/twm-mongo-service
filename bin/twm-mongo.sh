c8b3683295b5f5dac126644e5e5a992c92c272ad
Vivek <vivektheintel@gmail.com>
2024-08-11 13:10:02 -0400

    Upgrading scripts and libs

--- bin/twm-mongo.sh
+++ bin/twm-mongo.sh
@@ -1,11 +1,10 @@
 APP_NAME="twm-mongo-service"
-APP_VERSION="2.0-SNAPSHOT"
 JAVA_PARAM="-Xmx1g"
-TWM_PARAM=""
 
 BIN_PATH=$TWM_HOME_PARENT/TWM/$APP_NAME/bin     #TWM-HOME-PARENT :: exported in .bashrc
-JAR_PATH=$BIN_PATH/../target/$APP_NAME-$APP_VERSION.jar
-PARAMS=$JAVA_PARAM" "$TWM_PARAM
+cd $BIN_PATH/../target/
+JAR_NAME=`ls *jar`
+JAR_PATH=$BIN_PATH/../target/$JAR_NAME
 JAVA_PATH=$HOME/.jdks/jdk17/bin/java
 
 echo "Starting '$APP_NAME' with params: '$PARAMS', at '$JAR_PATH'"
